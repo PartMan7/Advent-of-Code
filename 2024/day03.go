eba17677283699f428046f70b7258d419fce30f2
Parth Mane <parth.mane@sprinklr.com>
2024-12-04 17:38:10 +0530

    2024: Day 3 update

--- 2024/day03.go
+++ 2024/day03.go
@@ -9,23 +9,23 @@ import (
 )
 
 func main() {
-	file, _ := os.ReadFile("day03.txt")
-	file = []byte(strings.ReplaceAll(string(file), "\n", ""))
+	newlinesFile, _ := os.ReadFile("day03.txt")
+	file := (strings.ReplaceAll(string(newlinesFile), "\n", ""))
 	mulExp := regexp.MustCompile(`mul\((\d+),(\d+)\)`)
 	doDontExp := regexp.MustCompile(`don't\(\).*?(do\(\)|$)`)
-	results1 := mulExp.FindAllSubmatch(file, -1)
-	trimmedFile := doDontExp.ReplaceAll(file, []byte("$1"))
-	results2 := mulExp.FindAllSubmatch(trimmedFile, -1)
+	results1 := mulExp.FindAllStringSubmatch(file, -1)
+	trimmedFile := string(doDontExp.ReplaceAll([]byte(file), []byte("$1")))
+	results2 := mulExp.FindAllStringSubmatch(trimmedFile, -1)
 	sum1 := 0
 	for _, result := range results1 {
-		num1, _ := strconv.Atoi(string(result[1]))
-		num2, _ := strconv.Atoi(string(result[2]))
+		num1, _ := strconv.Atoi((result[1]))
+		num2, _ := strconv.Atoi((result[2]))
 		sum1 += num1 * num2
 	}
 	sum2 := 0
 	for _, result := range results2 {
-		num1, _ := strconv.Atoi(string(result[1]))
-		num2, _ := strconv.Atoi(string(result[2]))
+		num1, _ := strconv.Atoi((result[1]))
+		num2, _ := strconv.Atoi((result[2]))
 		sum2 += num1 * num2
 	}
 	fmt.Println(sum1, sum2)
